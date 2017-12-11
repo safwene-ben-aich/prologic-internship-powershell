7ce26b3f890e8dddf55b7924faeea5955f046d43
Safwene Ben Aich <safwene.benaich@gmail.com>
2017-12-11 16:38:19 +0100

    first commit

---
+++ InterConnectVI.ps1
@@ -0,0 +1,122 @@
+﻿
+
+
+Function Inter-ConnectVI{
+
+        $serverName = Read-Host "Enter the server name "
+        
+        if (!(Inter-getOnlineTest  -ComputerName $serverName)){
+            return
+        }
+        
+        if (! (Inter-checkActiveConnection -serverName $serverName )){
+            $userName   = Read-Host "Enter the username "
+            $password = Read-Host  "Enter the password" -AsSecureString
+            if (Inter-ConnectVIServer -serverName $serverName -userName $userName -password $password){
+                #Call the menu Function
+                Inter-Menu
+            }
+            else {
+                return
+            }            
+        
+        }
+
+        else {
+            #Call the menu Function
+            Inter-Menu
+
+        }
+
+}
+
+
+
+Function Inter-getOnlineTest(){
+
+    Param(
+        # Name of the computer
+        $ComputerName
+        )
+    Write-Host -NoNewline "Testing connection with $ComputerName, Please wait... "
+    # Test Function
+    sleep -Seconds 1
+    $onlineTest = Test-Connection -ComputerName  $ComputerName -Count 1 -Verbose -Quiet
+        if ($onlineTest -eq $false){
+            Write-Host "[Host is offline]"
+            return $false
+        }
+        else {
+        
+            Write-Host "[OK]"
+        }
+    return $true
+}
+
+
+Function Inter-checkActiveConnection(){
+    
+    Param(
+    #Param to get the server informations
+    $serverName
+    )
+    
+    Write-Host -NoNewline  "Checking for active connections with $serverName, Please wait... "
+    $numberServers = $global:DefaultVIServers.Count
+    sleep -Seconds 1
+    
+    
+    if ($numberServers -gt 0){
+        Write-Host "[OK]"
+        Write-Host "You are already connected to one or more server(s)"    
+        foreach ($server in $global:DefaultVIServers){
+         $server | Format-Table | Out-Default
+        }
+       return $true
+    }
+    Write-Host "[No Valid connections found]"
+    return $false
+}
+
+
+
+Function Inter-ConnectVIServer(){
+Param (
+    $serverName,
+    $userName,
+    $password
+)
+
+$unsecurePassword = (New-Object PSCredential "user",$password).GetNetworkCredential().Password
+Connect-VIServer -Server $serverName -User $userName -Password $unsecurePassword -ErrorAction SilentlyContinue | Out-Default 
+if ($global:DefaultVIServer -eq $null){
+    Write-Host "Cannot connect to this server, Maybe wrong username or password ?"
+    return $false
+}
+
+return $true
+
+}
+
+
+
+Function Inter-Menu(){
+Write-Host "1.VMs Scenarios`n2.Exit`nSelect menu item: " -NoNewline
+$userChoice = Read-Host
+if ($userChoice -eq 1){
+    inter-VMAutomationMenu
+}
+elseif($userChoice -eq 2){
+    Exit
+}
+}
+
+
+
+
+
+
+
+clear
+
+Inter-ConnectVI
---
+++ InterVMAutomation.ps1
@@ -0,0 +1,73 @@
+﻿
+Function inter-VMAutomationMenu{
+Write-Host "1.Start up All VM
+2.Shutdown All VM
+3.HealthCheck
+10. Return
+Select menu item: " -NoNewline
+$userChoice = Read-Host    
+if ($userChoice -eq 1){
+    inter-StartUpVMs
+}
+elseif ($userChoice -eq 2){
+    inter-StopVMs
+}
+elseif ($userChoice -eq 3){
+    inter-HealthCheck
+}
+}
+
+Function inter-StartUpVMs{
+$VMs = Get-VM
+$VMs | foreach {
+Write-Host "Starting $($_.name)"
+$StaringVMs = Start-VM $_ -Confirm:$false -RunAsync
+}
+
+$VMs | Foreach {
+do {
+$VMs = Get-VM $_
+$Toolsstatus = $VMs.ExtensionData.Guest.ToolsRunningStatus
+Write-Host "Waiting for $VMs to start, tools status is $Toolsstatus"
+Sleep 7
+} until ($Toolsstatus -eq "guestToolsRunning")
+}
+}
+
+
+Function inter-StopVMs{
+$VMs = Get-VM
+$VMs | foreach {
+if ($_.Name -ne "VMware vCenter Server Appliance"){
+Write-Host "Stoping $($_.name)"
+$StaringVMs = Stop-VM $_ -Confirm:$false -RunAsync
+}
+}
+$VMs | Foreach {
+if (-ne "VMware vCenter Server Appliance"){
+do {
+$VMs = Get-VM $_
+$Toolsstatus = $VMs.ExtensionData.Guest.ToolsRunningStatus
+Write-Host "Waiting for $VMs to stop, tools status is $Toolsstatus"
+Sleep 7
+} until ($Toolsstatus -eq "guestToolsNotRunning")
+}
+}
+}
+
+
+Function inter-HealthCheck{
+Get-VM  | Get-View | Select Name,@{Name=”Hardware Version”; Expression={$_.Config.Version}},
+@{Name=”Tools Status”; Expression={$_.Guest.ToolsStatus}},
+@{Name=”Tools Version Status”; Expression={$_.Guest.ToolsVersionStatus}},
+@{Name=”Tools Version”; Expression={$_.Guest.ToolsVersion}},
+@{Name=”Snapshots”; Expression={(((Get-Snapshot -VM (Get-VM -Name $_.Name)).Name)).Length -ne 0}},
+@{Name=”Data Store path”; Expression={((((Get-VM -Name $_.Name)).DatastoreIdList)).Length -ne 0}}
+}
+
+Function inter-TEST{
+
+}
+inter-VMAutomationMenu
+
+
